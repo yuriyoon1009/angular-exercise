840f654832c399982e3d9cc771e95e915b7ae3f8
Seokju Na <seokju.me@gmail.com>
2018-01-23 17:47:35 +0900

    Update imports order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { TodoModule } from './todo/todo.module';
 import { CoreModule } from './core/core.module';
